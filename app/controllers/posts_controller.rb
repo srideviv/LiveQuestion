3f3893d23bdd51937de6ba47e5a63227312ab50a
Sridevi Venugopal Thirumala Iyengar <sthirum@ncsu.edu>
2011-10-07 21:06:31 -0400

    changing quotes on order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.find(:all,:order => "numVotes DESC, created_at DESC", :limit => "5")
+    @posts = Post.find(:all,:order => 'numVotes DESC, created_at DESC', :limit => 5)
 
     respond_to do |format|
       format.html # index.html.erb
